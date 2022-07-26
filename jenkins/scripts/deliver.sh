50a364262d474e69f6f093aa3d6e46cfc36cae72
Fikri Helmi Setiawan <fikrihelmi17@gmail.com>
2022-07-26 14:19:17 +0700

    port change

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,6 +24,6 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3030 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3030:3030''" parameter when you'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
